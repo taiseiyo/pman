59e7f331c8e4d76cb7860ce6cc7b2d4ee32e6cb9
suzuki taisei <taisei@localhost>
2022-01-30 00:18:42 +0900

    fix

--- pman.el
+++ pman.el
@@ -3,6 +3,17 @@
   (interactive)
   (setq name (read-string "enter -m module or -f function : "))
   (setq command (concat "env python3 ~/bin/pman.py " name))
-  (compile command))
-				    
+  (prepare_window)
+  (compile command)
+  )
+
+(defun prepare_window ()
+  (setq split_flag (one-window-p))
+  (if (eq t split_flag)
+      (progn 
+	(split-window-horizontally))
+  )
+  )
+
 (provide 'pman)
+
