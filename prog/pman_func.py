54603f401a74b7cdb749331f5baaa099ea97d603
suzuki taisei <taisei@localhost>
2022-02-19 10:00:30 +0900

    fix

--- prog/pman_func.py
+++ prog/pman_func.py
@@ -26,8 +26,9 @@ def option_parser():
 
 
 def html_parser(url):
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
+    # with urllib.request.urlopen(url) as response:
+    response = urllib.request.urlopen(url)
+    html = response.read()
     soup = BeautifulSoup(html, "lxml")
     print(soup.get_text().strip())
 
